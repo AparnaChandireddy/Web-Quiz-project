2485f48d2645cd16c4a08c3e0637ba006cb865ef
Chandireddy Navya <navyach158@gmail.com>
2024-04-18 13:56:59 -0400

    Project

---
+++ questions.js
@@ -0,0 +1,48 @@
+let questions = [
+   {
+    numb: 1,
+    question: "Which of the following attributes is used to add link to any element?",
+    answer: "href",
+    options: [
+     "link",
+     "href",
+     "ref",
+     "newref"
+    ]
+},
+   { 
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+    "Hyper Text Markup Language",
+    "Hyper Text Preprocessor",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+    ]
+},
+{
+    
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hyper Text Preprocessor",
+    options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Programming",
+    "Hyper Text PreProgramming",
+    "Home Text Preprocessor"
+    ]
+},
+{
+    numb: 4,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+        "Common Style Sheet",
+        "Colourful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+    ]
+}
+];
+
